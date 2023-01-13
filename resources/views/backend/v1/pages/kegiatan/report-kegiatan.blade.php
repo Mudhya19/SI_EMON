add6a2d0bcf23fa6f83a12007cf1c63eadaeb918
Mudhya19 <muhammaddhiauddin40@gmail.com>
2023-01-13 14:18:40 +0800

    commit

--- resources/views/backend/v1/pages/kegiatan/report-kegiatan.blade.php
+++ resources/views/backend/v1/pages/kegiatan/report-kegiatan.blade.php
@@ -57,8 +57,8 @@
                 <th>Nama Kegiatan</th>
                 <th>Indikator</th>
                 <th>Target</th>
-                <th>Pagu</th>
                 <th>Satuan</th>
+                <th>Pagu</th>
                 <th>Penanggung jawab</th>
             </tr>
             @foreach ($kegiatans as $kegiatan)
@@ -97,11 +97,11 @@
                     'Desember',
                 ];
                 $pecahkan = explode('-', $tanggal);
-            
+
                 // variabel pecahkan 0 = tanggal
                 // variabel pecahkan 1 = bulan
                 // variabel pecahkan 2 = tahun
-            
+
                 return $pecahkan[2] . ' ' . $bulan[(int) $pecahkan[1]] . ' ' . $pecahkan[0];
             }
             echo tgl_indo(date(' Y-m-d')); ?></p>
